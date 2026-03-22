0a3d67d7ff42876bb0498757ee410593d9241590
ismail <mdismail5845@gmail.com>
2026-03-22 21:59:52 +0600

    First commit: Dynamic Project

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name =  models.CharField(max_length=20)
     
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
     def __str__(self):
         return self.name
 
@@ -12,11 +15,17 @@ class ProjectSection(models.Model):
     title = models.CharField(max_length=50)
     subtitle = models.TextField()
     
+    class Meta:
+        verbose_name_plural = 'Project Section'
+    
     def __str__(self):
         return self.title
     
 class Projects(models.Model):
     name = models.CharField(max_length=30)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(upload_to='projects/images', blank=True, null=True)
     category =  models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+    
+    class Meta:
+        verbose_name_plural = 'Projects'
     
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -4,18 +4,6 @@ from apps.projects.models import (
 )
 
 # Create your views here.
-# def projects(request):
-#     categories = Category.objects.all()
-#     project_section = ProjectSection.objects.first()
-#     category_id = request.GET.get('category')
-#     if not category_id or category_id == 'all':
-#         projects = Projects.objects.select_related('category').all()
-#     else:
-#         projects = Projects.objects.filter(category_id = category_id)
-#     context = {
-#         'categories' : categories,
-#         'project_section' : project_section,
-#         'projects' : projects,
-#         'active_category' : category_id,
-#     }
-#     return render(request, 'projects.html', context)
+
+# Project display and filtering logic is implemented in:
+# apps.projects.templatetags.project_tags.py
